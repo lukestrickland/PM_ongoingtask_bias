1a0cf51beb1ba982875e4ede2ac45ce5b10f69ab
Luke Strickland <ljs392@cl2.newcastle.edu.au>
2019-04-24 18:34:27 +1000

    new graphs to visualize drift criterion effects

--- R/4-posterior_inference.R
+++ R/4-posterior_inference.R
@@ -4,7 +4,7 @@ source ("R/0-functions.R")
 load_model ("LBA","lbaN_B.R")
 load("samples/samples_top.RData")
 
-msds <- get.msds(samples_top)
+msds <- get.msds(samples_top[definitely_biased])
 msds <- label_msds(msds)
 
 Bs <- msds[grep("B", rownames(msds)),]
@@ -36,7 +36,20 @@ ggplot(mvs, aes(factor(S),M)) +
 ggplot(mvs[mvs$R!="PM",], aes(E,M)) + 
   geom_point(aes(shape=day, col= isPM), size=3) +
   geom_errorbar(aes(ymax = M + SD, ymin = M - SD, width = 0.2, col=isPM))+
-  ylab("Accumulation Rate") + ylim(-2, 2.5) + 
+  ylab("Accumulation Rate") +
   geom_line(aes(group=interaction(day, isPM), col=isPM, y=M), linetype=2) +
-  facet_grid(ot_match~ot_correct)
+  facet_grid(ot_match~ot_correct, scales="free")
 
+ggplot(mvs[mvs$R!="PM" &mvs$isPM=="PM",], aes(E,M)) + 
+  geom_point(aes(shape=day, col= isPM), size=3) +
+  geom_errorbar(aes(ymax = M + SD, ymin = M - SD, width = 0.2, col=isPM))+
+  ylab("Accumulation Rate") +
+  geom_line(aes(group=interaction(day, isPM), col=isPM, y=M), linetype=2) +
+  facet_grid(ot_match~ot_correct, scales="free")
+
+ggplot(mvs[mvs$R!="PM" &mvs$isPM=="nonPM",], aes(E,M)) + 
+  geom_point(aes(shape=day, col= isPM), size=3) +
+  geom_errorbar(aes(ymax = M + SD, ymin = M - SD, width = 0.2, col=isPM))+
+  ylab("Accumulation Rate") +
+  geom_line(aes(group=interaction(day, isPM), col=isPM, y=M), linetype=2) +
+  facet_grid(ot_match~ot_correct, scales="free")
